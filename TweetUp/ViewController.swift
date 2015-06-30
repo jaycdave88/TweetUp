179a77ea45c0fbe05820a79c573e046e4324508c
Jay Dave <jaycdave@gmail.com>
2015-06-29 20:23:02 -0700

    first commit pushing empty repo to Github - with readme file

---
+++ TweetUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TweetUp
+//
+//  Created by DEV MODE on 6/29/15.
+//  Copyright (c) 2015 DEV MODE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+###TweetUp:
+* A iOS application written in swift that will allow you to add text to your profile picture for Twitter and reupload the new image.
+    
+> Jay C Davé
